e884b26927ca678d35eaf26455213762cab936ad
iamvickyav <vickyavw.10@gmail.com>
2020-08-08 11:37:41 +0530

    POST, PUT, RequestBody, HTTP Status 201, Iterator added

--- src/main/java/com/vicky/itechapp/EmployeeController.java
+++ src/main/java/com/vicky/itechapp/EmployeeController.java
@@ -4,17 +4,14 @@ import com.vicky.itechapp.model.Employee;
 import com.vicky.itechapp.model.Role;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class EmployeeController {
+
+    List<Employee> employeeList = new ArrayList<>();
 
     @RequestMapping(value = "/employee/all")
     List<Employee> getEmployees(){
@@ -59,32 +56,85 @@ public class EmployeeController {
         return responseEntity;
     }
 
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
+    public ResponseEntity createEmployee(@RequestBody Employee employee){
+        addEmployee(employee);
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "Employee added succesfully");
+        ResponseEntity responseEntity = new ResponseEntity(map, HttpStatus.CREATED);
+        return responseEntity;
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.PUT)
+    public ResponseEntity updateEmployee(@RequestBody Employee employee) {
+        boolean updated = updateMyEmployee(employee);
+        Map<String, String> map = new HashMap<>();
+        ResponseEntity responseEntity;
+
+        if(updated) {
+            map.put("message", "Employee updated succesfully");
+             responseEntity = new ResponseEntity(map, HttpStatus.OK);
+        } else {
+            map.put("message", "Employee not found for update");
+            responseEntity = new ResponseEntity(map, HttpStatus.NOT_FOUND);
+        }
+
+        return responseEntity;
+    }
 
 
     private List<Employee> prepareData() {
-        Employee e1 = new Employee();
-        e1.setId(1);
-        e1.setName("Vidhya");
 
-        Role role = new Role();
-        role.setDesignation("Chief Engineer");
-        role.setDept("IT Dept");
+        if(employeeList.isEmpty()) {
+            Employee e1 = new Employee();
+            e1.setId(1);
+            e1.setName("Vidhya");
 
-        e1.setRole(role);
+            Role role = new Role();
+            role.setDesignation("Chief Engineer");
+            role.setDept("IT Dept");
 
-        Employee e2 = new Employee();
-        e2.setId(2);
-        e2.setName("Vandhana");
+            e1.setRole(role);
 
-        Role role2 = new Role();
-        role2.setDesignation("The CEO");
-        role2.setDept("Management");
-        e2.setRole(role2);
+            Employee e2 = new Employee();
+            e2.setId(2);
+            e2.setName("Vandhana");
 
-        List<Employee> employeeList = new ArrayList<>();
-        employeeList.add(e1);
-        employeeList.add(e2);
+            Role role2 = new Role();
+            role2.setDesignation("The CEO");
+            role2.setDept("Management");
+            e2.setRole(role2);
 
+            employeeList.add(e1);
+            employeeList.add(e2);
+        }
         return employeeList;
+    }
+
+    private void addEmployee(Employee employee) {
+        List<Employee> employeeList = prepareData();
+        // TODO: Make sure you are checking before adding
+        // TODO: Condition - Check if Employee already exist by using Id
+        employeeList.add(employee);
+    }
+
+    private boolean updateMyEmployee(Employee tobeUpdatedEmployee) {
+        List<Employee> employeeList = prepareData();
+
+        Iterator<Employee> iterator = employeeList.iterator();
+        boolean removed = false;
+
+        while(iterator.hasNext()) {
+            Employee employee = iterator.next();
+            if(employee.getId() == tobeUpdatedEmployee.getId()) {
+                iterator.remove();
+                removed = true;
+                break;
+            }
+        }
+        if(removed) {
+            employeeList.add(tobeUpdatedEmployee);
+        }
+        return removed;
     }
 }
